8832221f398cb392c88cc7bf48c23327dbf8c549
nexcontelecom <nexcontelecom@gmail.com>
2021-09-09 14:07:24 +0900

    ㅅest

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useRef, useEffect, useState,useCallback } from "react";
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import {HashRouter as Router,Switch,Route} from "react-router-dom";
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
 import AboutUs from "./components/about/about";
